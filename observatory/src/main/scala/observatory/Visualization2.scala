f287be7101d11e1e1d0e8d876cbd541345a63aaf
Christian Mösl <christian.moesl@live.at>
2020-01-05 21:44:43 +0100

    Milestone 3

--- observatory/src/main/scala/observatory/Visualization2.scala
+++ observatory/src/main/scala/observatory/Visualization2.scala
@@ -1,11 +1,14 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import math.{sqrt, pow, ceil, floor, abs}
 
 /**
   * 5th milestone: value-added information visualization
   */
 object Visualization2 extends Visualization2Interface {
+  import Interaction._
+  import Visualization._
 
   /**
     * @param point (x, y) coordinates of a point in the grid cell
@@ -23,7 +26,10 @@ object Visualization2 extends Visualization2Interface {
     d10: Temperature,
     d11: Temperature
   ): Temperature = {
-    ???
+    d00 * (1 - point.x) * (1 - point.y) + 
+    d10 * point.x * (1 - point.y) + 
+    d01 * point.y * (1 - point.x) + 
+    d11 * point.x * point.y
   }
 
   /**
@@ -37,7 +43,29 @@ object Visualization2 extends Visualization2Interface {
     colors: Iterable[(Temperature, Color)],
     tile: Tile
   ): Image = {
-    ???
+    def interpolateWithGrid(location: Location): Temperature = {
+      val lat = if (location.lat >= 0) ceil(location.lat).toInt else location.lat.toInt
+      val lon = if (location.lon >= 0) location.lon.toInt else floor(location.lon).toInt
+
+      val point = CellPoint(abs(location.lon - lon), abs(location.lat - lat))
+      val d00 = grid(GridLocation(lat, lon))
+      val d01 = grid(GridLocation(lat - 1, lon))
+      val d10 = grid(GridLocation(lat, lon + 1))
+      val d11 = grid(GridLocation(lat - 1, lon + 1))
+
+      bilinearInterpolation(point, d00, d01, d10, d11)
+    }
+
+    val pixels = tilePixelLocations(tile).par
+      .map(interpolateWithGrid)
+      .map(interpolateColor(colors, _))
+      .map{ case Color(r, g, b) => Pixel(r, g, b, 127) }
+      .toArray
+
+    val width = sqrt(pixels.size).toInt
+    val height = width
+
+    Image(width, height, pixels)
   }
 
 }
